665a93e340c61938e7eec46e360b0a2a66873a10
yasin ünal <yasinunalnn@gmail.com>
2020-12-21 21:36:40 +0300

    Add project files.

---
+++ WebProgrammingProject/Data/Migrations/20201221174228_MotorcycleCultureMig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebProgrammingProject.Data.Migrations
+{
+    public partial class MotorcycleCultureMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
